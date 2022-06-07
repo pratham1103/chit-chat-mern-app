62398778ee35b909ca59f2624199a11eb5e326c5
pratham1103 <pratham.mittal.met20@itbhu.ac.in>
2022-06-07 23:23:12 +0530

    make it better

--- chit-chat/src/components/SingleChat.js
+++ chit-chat/src/components/SingleChat.js
@@ -71,7 +71,9 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   };
 
   useEffect(() => {
-    socket = io(ENDPOINT);
+    socket = io(ENDPOINT, {
+      transports: ["websocket", "polling", "flashsocket"],
+    });
     socket.emit("setup", user);
     socket.on("connected", () => setSocketConnected(true));
     socket.on("typing", () => setIsTyping(true));
