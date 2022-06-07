c7dabfca08b8d95134bc49b5973929299cc08050
pratham1103 <pratham.mittal.met20@itbhu.ac.in>
2022-06-07 19:26:54 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://chit-chat-pm11.herokuapp.com/",
+    origin: "*",
   },
 });
 
