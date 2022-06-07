8555e9b2351310838f430c049fbe561e9baf6b81
pratham1103 <pratham.mittal.met20@itbhu.ac.in>
2022-06-08 01:32:55 +0530

    make it better

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -11,15 +11,6 @@ const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
