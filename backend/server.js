04c831e907224f5b713b1301f2911ced4d62acb2
pratham1103 <pratham.mittal.met20@itbhu.ac.in>
2022-06-08 14:38:28 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -52,8 +52,8 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: true,
-    credentials: true,
+    origin: "http://localhost:3000",
+    // credentials: true,
   },
 });
 
@@ -71,15 +71,15 @@ io.on("connection", (socket) => {
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+  socket.on("new message", (newMessageReceived) => {
+    var chat = newMessageReceived.chat;
 
     if (!chat.users) return console.log("chat.users not defined");
 
     chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
+      if (user._id == newMessageReceived.sender._id) return;
 
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
+      socket.in(user._id).emit("message received", newMessageReceived);
     });
   });
 
