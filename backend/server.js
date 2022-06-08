83c82bc8bbca4cec76dadeadc22537a6a8d4a3d0
pratham1103 <pratham.mittal.met20@itbhu.ac.in>
2022-06-08 14:58:18 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,11 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/chit-chat/build")));
 
-  app.get("*", cors(corsOptions), (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "chit-chat", "build", "index.html"))
   );
 } else {
-  app.get("/", cors(corsOptions), (req, res) => {
+  app.get("/", (req, res) => {
     res.send("API is running..");
   });
 }
